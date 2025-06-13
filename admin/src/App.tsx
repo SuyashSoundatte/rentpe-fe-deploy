98549e6df5d88b2f0dec4b94d9762a3c50fcf119
SuyashSoundatte <suyashsoundatte7@gmail.com>
2025-06-13 15:59:55 +0530

    init

---
+++ admin/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="h-screen bg-[#212121] flex items-center justify-center">
+      <div className="text-white text-4xl">
+        <h1 className="bg-amber-300 p-5 rounded-3xl">Admin Website</h1>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
---
+++ vendor/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="h-screen bg-[#212121] flex items-center justify-center">
+      <div className="text-white text-4xl">
+        <h1 className="bg-blue-400 p-5 rounded-3xl">Vendor Website</h1>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
